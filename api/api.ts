e23450537d3585f3295cabbef92396901b45f1ca
Gergő <eles.gergely@outlook.com>
2022-07-27 19:46:47 +0200

    new version of client

--- api/api.ts
+++ api/api.ts
@@ -2,4 +2,7 @@ export * from './clientManagement.service';
 import { ClientManagementService } from './clientManagement.service';
 export * from './default.service';
 import { DefaultService } from './default.service';
-export const APIS = [ClientManagementService, DefaultService];
+export const APIS = [
+  ClientManagementService,
+  DefaultService,
+];
