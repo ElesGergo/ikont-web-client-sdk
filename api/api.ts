a6d840d7a89ed265653a451b84d0f8d921e709d5
Gergő <eles.gergely@sharpwebsolutoins.hu>
2022-08-18 00:07:42 +0200

    v.0.1.1

--- api/api.ts
+++ api/api.ts
@@ -2,13 +2,10 @@ export * from './authentication.service';
 import { AuthenticationService } from './authentication.service';
 export * from './clientManagement.service';
 import { ClientManagementService } from './clientManagement.service';
-export * from './default.service';
-import { DefaultService } from './default.service';
 export * from './location.service';
 import { LocationService } from './location.service';
 export const APIS = [
   AuthenticationService,
   ClientManagementService,
-  DefaultService,
   LocationService,
 ];
